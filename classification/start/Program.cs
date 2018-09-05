fa978411d8734dc851b4dd33482015cf12e9597c
Lucian Braescu <braescu_lucian@yahoo.com>
2018-09-06 00:16:06 +0300

    Added start/end classification

---
+++ classification/start/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+namespace classification
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Hello World!");
+        }
+    }
+}
