a3a7c2d7869cb6e839069d06a73f36834100a53d
Lucian Braescu <lucian.braescu@ascom.com>
2018-09-06 15:39:56 +0300

    Added api sample

---
+++ api/Controllers/PredictController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using api.Models;
+using Microsoft.ML;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string text)
+        {
+            var model = PredictionModel.ReadAsync<SentimentData, SentimentPrediction>("Model.zip").Result;
+            var prediction = model.Predict(new SentimentData { SentimentText = text });
+            return prediction.Sentiment ? "Positive" : "Negative";
+        }
+    }
+}
